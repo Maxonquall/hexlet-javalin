2538467474169af156df044c4a6418aefb9b8b1e
Maxim <msirotkin95@gmail.com>
2024-04-09 16:08:20 +0300

    Javalin

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,10 +4,19 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаем приложение
-        var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
-        // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParam("name");
+            if (name != null && !name.isEmpty()) {
+                ctx.result("Hello, " + name + "!");
+            } else {
+                ctx.result("Hello, World!");
+            }
+        });
+        app.start(7070);
     }
 }
