2ff07f0d7cbca62b5c5768d7224f7df4cd1f22e3
Maxim <msirotkin95@gmail.com>
2024-04-11 10:11:23 +0300

    Javalin

---
+++ src/org/example/hexlet/HelloWorld.java
@@ -0,0 +1,25 @@
+package org.example.hexlet;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import static io.javalin.rendering.template.TemplateUtil.model;
+import org.example.hexlet.model.Course;
+import org.example.hexlet.dto.courses.CoursesPage;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/courses", ctx -> {
+            var courses = /* Список курсов извлекается из базы данных */
+                    var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", model("page", page));
+        });
+
+        app.start(7070);
+    }
+}
